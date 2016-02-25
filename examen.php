a1154a726a3ba39194aaab2d19ead08822835dd2
edgar flores <edfloco1997@gmail.com>
2016-02-25 16:17:13 -0500

    evaluacion

---
+++ examen.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>esto es</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+<marquee>
+	<h1>evaluando esto</h1>
+	</marquee>
+</body>
+</html>
